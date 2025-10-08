2fa252a68b28cf8c2043e6ba7ef4d8bf300163c0
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-10-08 20:59:51 +0800

    refactor: make a navbar component to concise the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./globals.css";
 import type { RootLayoutProps } from "./types";
-import Link from "next/link";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
-import PrintForgeLogoIcon from "@/public/printforge-logo-icon.svg";
-import PrintForgeLogo from "@/public/printforge-logo.svg";
+import NavBar from "./components/NavBar";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -17,40 +15,13 @@ const montserratAlternates = Montserrat_Alternates({
   variable: "--font-montserrat-alternates",
 });
 
-export default function RootLayout({
-  children,
-}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
         className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
-        <header className="w-full bg-white">
-          <nav className="flex justify-between px-6 py-4">
-            <Link href="/">
-              <div className="relative">
-                <img
-                  className="w-[200px] h-auto hidden md:block"
-                  src={PrintForgeLogo.src}
-                  alt="Desktop logo"
-                />
-                <img
-                  className="w-[40px] h-auto block md:hidden"
-                  src={PrintForgeLogoIcon.src}
-                  alt="Mobile logo"
-                />
-              </div>
-            </Link>
-            <ul className="flex items-center gap-2.5">
-              <li className="transition duration-100 hover:text-[#F77429] border-[#F77429] hover:border-b-1">
-                <Link href="/3d-models">3D MODELS</Link>
-              </li>
-              <li className="transition duration-100 hover:text-[#F77429] border-[#F77429] hover:border-b-1">
-                <Link href="/about">ABOUT</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <NavBar />
         {children}
       </body>
     </html>
