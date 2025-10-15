c14d2ef8b8ea8180af39b7821b94cd7dec2631a7
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-10-15 20:29:14 +0800

    this is the helper function for categories

---
+++ app/lib/categories.ts
@@ -0,0 +1,21 @@
+import categories from "../data/categories.json";
+import type { Category } from "../types";
+
+export function getAllCategories(): Category[] {
+    return categories
+}
+
+export function getCategoriesBySlug(slug: string): Category{
+    const category = categories.find(cat => cat.slug === slug)
+
+    if(!category){
+        throw new Error(`Category with slug ${slug} not found`)
+    }
+
+    return category
+}
+
+export function getDisplayNameFromSlug(slug: string): string {
+    const category = getCategoriesBySlug(slug)
+    return category.displayName
+}
