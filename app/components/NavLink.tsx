60075cce5ebf35b641c4de9f7e3316f63e8c08c0
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-10-19 23:45:51 +0800

    component for link and use isActive for active link

---
+++ app/components/NavLink.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import type { NavLinkProps } from "../types";
+export default function NavLink({ href, isActive, children }: NavLinkProps) {
+  return (
+    <li className="text-sm uppercase">
+      <Link
+        href={href}
+        className={`px-4 py-2 transition-colors rounded-md cursor-pointer hover:text-[#F77429] ${
+          isActive ? "text-[#F77429]" : "text-gray-700"
+        }`}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
