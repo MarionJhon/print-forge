62e5a0db469bda3fe90fdcad0855ee6c500fcc48
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-10-19 23:38:16 +0800

    code indentation

--- app/lib/categories.ts
+++ app/lib/categories.ts
@@ -2,20 +2,20 @@ import categories from "../data/categories.json";
 import type { Category } from "../types";
 
 export function getAllCategories(): Category[] {
-    return categories
+  return categories;
 }
 
-export function getCategoriesBySlug(slug: string): Category{
-    const category = categories.find(cat => cat.slug === slug)
+export function getCategoriesBySlug(slug: string): Category {
+  const category = categories.find((cat) => cat.slug === slug);
 
-    if(!category){
-        throw new Error(`Category with slug ${slug} not found`)
-    }
+  if (!category) {
+    throw new Error(`Category with slug ${slug} not found`);
+  }
 
-    return category
+  return category;
 }
 
 export function getDisplayNameFromSlug(slug: string): string {
-    const category = getCategoriesBySlug(slug)
-    return category.displayName
-}
+  const category = getCategoriesBySlug(slug);
+  return category.displayName;
+}   
