c38ad6860962f3ada48ecf2b1e7664997cf1811f
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-10-22 14:23:14 +0800

    chore: searchParams and filtered the query to get the what you search

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,8 +1,25 @@
 import { getModels } from "../lib/models";
 import ModelGrid from "../components/ModelGrid";
+import { ModelPageProps } from "../types";
+import Form from 'next/form'
 
-export default async function ModelListPage() {
+export default async function ModelListPage({ searchParams }: ModelPageProps) {
+  const query = (await searchParams)?.query?.toLowerCase() || "";
   const models = await getModels({});
+  const filteredModel = query
+    ? models.filter(
+        (obj) =>
+          obj.name.toLowerCase().includes(query) ||
+          obj.description.toLowerCase().includes(query)
+      )
+    : models;
 
-  return <ModelGrid title="3D Models" models={models} />
+  return (
+    <>
+      <Form action="/3d-models" className="w-full px-5 md:px-0 md:max-w-xl">
+        <input type="text" name="query" placeholder="Search" autoComplete="off" defaultValue={query} className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none"/>
+      </Form>
+      <ModelGrid title="3D Models" models={filteredModel} />
+    </>
+  );
 }
